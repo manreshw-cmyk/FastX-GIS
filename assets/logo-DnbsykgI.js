baa05a4f47c4af06c3b2901d5ee58629842d1d64
manreshw-cmyk <manre.shw@gmail.com>
2026-05-28 10:13:49 +0000

    Deploying to gh-pages from @ manreshw-cmyk/FastX-GIS@09f5717e92acdd45f3953f8979a6a9d10d406aeb 🚀

---
+++ assets/logo-DnbsykgI.js
@@ -0,0 +1,1 @@
+const s="/FastX-GIS/assets/logo-rZoIvfC-.png";export{s as _};
---
+++ assets/useAreaManagerPublish-X3N0sPDy.js
@@ -0,0 +1,1 @@
+function t(r,i,s){var n;const a=(n=window.FastX)==null?void 0:n.AreaManager;a&&a.publish(e=>{e.shapeType===r&&(s(),i(e))})}export{t as b};
