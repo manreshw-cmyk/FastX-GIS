50c6ef7d520b1c427b55dc4a92006d7c0f0376c0
manre <3118690231@qq.com>
2026-07-01 20:23:45 +0800

    fix:特效集合-新增环形雷达扫描

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 /** 浏览器全局配置（由 index.html 先于应用脚本加载）。 */
 window.apiConfig = {
-  imageryProvider: 'http://localhost:98/qqhr_satellite/{z}/{x}/{y}.png',
+  // imageryProvider: 'http://localhost:98/qqhr_satellite/{z}/{x}/{y}.png',
   // terrainProvider: 'http://localhost:98/taiwan_dem',
-  // imageryProvider: 'https://manreshw-cmyk.github.io/FastX-GIS/map/tianditu_Image/{z}/{x}/{y}.png',
+  imageryProvider: 'https://manreshw-cmyk.github.io/FastX-GIS/map/tianditu_Image/{z}/{x}/{y}.png',
   terrainProvider: '',
 }
